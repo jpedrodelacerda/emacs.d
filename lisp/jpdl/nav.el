e93541cf8c60a4d7bcffd2eb5b5bd2b5290ec2a3
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-05-29 13:03:30 -0300

    feat(nav): add windswap
    removes dead code and refactor windmove configuration

--- lisp/jpdl/nav.el
+++ lisp/jpdl/nav.el
@@ -7,27 +7,15 @@
 (require 'jpdl/utils)
 
 (use-package windmove
-  :bind (("M-<up>"    . windmove-up   )
-  		 ("M-<down>"  . windmove-down )
-  		 ("M-<right>" . windmove-right)
-  		 ("M-<left>"  . windmove-left )
-  		 ("C-M-r"     . split-window-horizontally-and-switch)
-  		 ("C-M-d"     . split-window-vertically-and-switch)
-         ))
+  :config
+  (windmove-default-keybindings 'meta)
+  :bind
+  ("C-M-r"     . split-window-horizontally-and-switch)
+  ("C-M-d"     . split-window-vertically-and-switch))
 
-(defun split-window-below-and-switch ()
-  "Split the window horizontally, then switch to the new pane."
-  (interactive)
-  (split-window-below)
-  (balance-windows)
-  (other-window 1))
-
-(defun split-window-right-and-switch ()
-  "Split the window vertically, then switch to the new pane."
-  (interactive)
-  (split-window-right)
-  (balance-windows)
-  (other-window 1))
+(use-package windswap
+  :config
+  (windswap-default-keybindings 'meta 'control))
 
 (global-set-key (kbd "C-x 2") 'split-window-below-and-switch)
 (global-set-key (kbd "C-x 3") 'split-window-right-and-switch)
