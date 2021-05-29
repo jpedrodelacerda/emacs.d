114d9506e187815109adf4bb2beb5e5ae027c3f6
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-05-29 19:09:49 -0300

    chore(smartparens): remove dead code

--- lisp/jpdl/smartparens.el
+++ lisp/jpdl/smartparens.el
@@ -6,19 +6,11 @@
 
 ;; tokens beutifier and completer
 (use-package smartparens
-  ;; :diminish smartparens-mode
-  ;; :commands smartparens-global-mode
   :bind
   ("C-SPC" . sp-forward-sexp)
   ("C-M-0" . smartparens-strict-mode)
-  ;; :commands (smartparens-mode show-smartparens-mode)
   :hook ((prog-mode . smartparens-strict-mode))
   :config
-  ;; (turn-on-smartparens-strict-mode)
-  ;; (smartparens-global-strict-mode t)
-  ;; (smartparens-global-mode 1)
-  ;; (smartparens-strict-mode 1)
-  ;; (setq smartparens-global-mode t)
   (show-smartparens-global-mode t)
   (require 'smartparens-config))
 
