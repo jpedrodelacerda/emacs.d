e8353e4a328c49df1d20c2421b5c8f10ba214ffc
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-05-29 12:29:35 -0300

    feat(helm): add bindings and ignore boring/.gitignored files

--- lisp/jpdl/helm.el
+++ lisp/jpdl/helm.el
@@ -10,12 +10,19 @@
   (helm-autoresize-mode 1)
   (setq helm-autoresize-min-height 25)
   (setq helm-autoresize-max-height 35)
+  ;; Ignore some files
+  (setq helm-ff-skip-boring-files t)
+  (setq helm-ff-skip-git-ignored-files t)
+
+  ;; bindings
   (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
   (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
   (define-key helm-map (kbd "C-z") #'helm-select-action)
   :bind (("C-x C-b" . helm-mini)
-	     ;; ("C-x C-f" . helm-find-files)
-	     ("C-x C-d" . helm-projectile-find-file)))
+	 ("C-x C-f" . helm-find-files)
+	 ("C-x 4 C-f" . helm-ff-find-file-other-tab)
+	 ("C-x 4 f" . helm-ff-find-file-other-tab)))
+
 
 (provide 'jpdl/helm)
 ;;; helm.el ends here
