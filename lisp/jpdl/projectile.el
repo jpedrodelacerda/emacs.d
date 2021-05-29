9a2ad12588985f70f6209aede52c8d5c49356b88
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-05-29 01:28:26 -0300

    feat(projectile): add projectile.el

---
+++ lisp/jpdl/projectile.el
@@ -0,0 +1,28 @@
+;;; projectile.el --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;; Handle projects
+(use-package projectile
+  :config
+  (projectile-mode 1)
+  (setq projectile-project-search-path '("~/"))
+  (setq projectile-completion-system 'helm)
+  (which-key-add-key-based-replacements "C-c p 4" "other-window"
+    "C-c p 5" "other-frame"
+    "C-c p s" "search"
+    "C-c p x" "execute")
+  ;; Set frame title
+  (setq frame-title-format '(concat ("emacs - ":eval (projectile-project-name))))
+  (setq projectile-indexing-method 'alien)
+  :bind-keymap ("C-c p" . projectile-command-map))
+
+(use-package helm-projectile
+  :after (helm projectile)
+  :init (helm-projectile-on)
+  :bind ("C-x C-f" . helm-projectile-find-file))
+
+(provide 'jpdl/projectile)
+;;; projectile.el ends here
