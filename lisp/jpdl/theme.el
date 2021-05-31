888f70a7787117a5a71f746358b5e81a82e9d033
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-05-30 21:15:00 -0300

    feat(theme): add configs and prepare for solaire-mode

--- lisp/jpdl/theme.el
+++ lisp/jpdl/theme.el
@@ -5,7 +5,20 @@
 ;;; Code:
 
 (use-package doom-themes
-  :config (load-theme 'doom-solarized-light))
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+  (load-theme 'doom-solarized-light)
+
+ ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  (setq doom-themes-treemacs-theme "doom-colors")
+  (setq doom-themes-treemacs-variable-pitch-face nil)
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (provide 'jpdl/theme)
 ;;; theme.el ends here
