b604f735d7ebd9d6b0d31be923272c9dfc5b947a
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2021-06-01 02:10:41 -0300

    feat(company): new binding and change order of company-backends

--- lisp/jpdl/company.el
+++ lisp/jpdl/company.el
@@ -10,11 +10,12 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-transformers '(company-sort-by-occurrence))
-  (setq company-backends (push '(:separate company-yasnippet company-capf) company-backends))
+  (setq company-backends (push '(:separate company-capf company-yasnippet) company-backends))
   :bind (("C-'" . company-complete)
 	 :map prog-mode-map
 	 ("<tab>" . company-indent-or-complete-common)
 	 :map company-active-map
+	 ("DEL" . company-abort)
 	 ("RET" . company-complete-selection)
 	 ("<tab>" . company-complete-common-or-cycle)
 	 ("<backtab>" . (lambda () (interactive) (company-complete-common-or-cycle -1)))))
