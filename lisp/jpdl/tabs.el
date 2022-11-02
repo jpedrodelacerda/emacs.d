c0fd7c76abd7b379a4dbcba29a329b0ebfde0469
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2022-11-01 22:13:58 -0300

    refactor(tabs): minor tweaks and new bindings

--- lisp/jpdl/tabs.el
+++ lisp/jpdl/tabs.el
@@ -5,23 +5,28 @@
 
 (use-package centaur-tabs
   :demand
-  :init (setq centaur-tabs-set-bar 'over)
   :config
   (centaur-tabs-mode +1)
+  (setq centaur-tabs-set-bar 'under)
+  (setq x-underline-at-descent-line t)
   (centaur-tabs-headline-match)
-  (setq centaur-tabs-set-modified-marker t
+  (setq centaur-tabs-style "wave"
+        centaur-tabs-set-modified-marker t
         centaur-tabs-modified-marker " ● "
         centaur-tabs-cycle-scope 'tabs
         centaur-tabs-height 30
         centaur-tabs-set-icons t
-        centaur-tabs-close-button " x ")
+        ;; centaur-tabs-show-navigation-buttons t
+        centaur-tabs-close-button "  ")
   (centaur-tabs-group-by-projectile-project)
   (setq centaur-tabs-cycle-scope 'tabs)
   :bind
   ;; ("<C-S-tab>" . centaur-tabs-backward)
   ("C-<iso-lefttab>" . centaur-tabs-backward)
   ("C-<tab>" . centaur-tabs-forward)
-  ("C-w" . kill-buffer))
+  ("C-w" . kill-buffer)
+  ("C-c C-<left>" . centaur-tabs-move-current-tab-to-left)
+  ("C-c C-<right>" . centaur-tabs-move-current-tab-to-right))
 
 (provide 'jpdl/tabs)
 ;;; tabs.el ends here
