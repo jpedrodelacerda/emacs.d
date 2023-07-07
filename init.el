126ebc4ff6078c6e725b77abeb90198649486400
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2023-07-07 01:04:04 -0300

    chore: reorder requires and remove missing `latex`

--- init.el
+++ init.el
@@ -21,28 +21,23 @@
 
 ;; Ensure that use-package is installed.
 
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
 (setq use-package-always-ensure t)
 
 ;; Booting
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(require 'jpdl/font)
+(require 'jpdl/system)
 (require 'jpdl/theme)
 (require 'jpdl/dashboard)
-(require 'jpdl/system)
-(require 'jpdl/font)
 (require 'jpdl/evil)
 (require 'jpdl/user)
 (require 'jpdl/ivy)
 (require 'jpdl/helm)
 (require 'jpdl/org)
-(require 'jpdl/latex)
 (require 'jpdl/agenda)
 (require 'jpdl/lsp)
 (require 'jpdl/tabs)
