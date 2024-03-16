ad139afbe4a6a0e38489e0be3da264a2a7d0f84a
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2024-03-16 18:44:55 -0300

    refactor(evil): update bindings, remove arrow nav

--- lisp/jpdl/evil.el
+++ lisp/jpdl/evil.el
@@ -8,18 +8,33 @@
   :straight t
   :init
   (setq evil-want-keybinding nil)
+  :bind
+  ("C-s" . evil-write)
+  ("C-o" . evil-jump-backward)
+  ("C-p" . evil-jump-forward)
   :config
   (evil-mode 1)
   (setq evil-emacs-state-mode nil)
   ;; Disable abbrev expansion when hit escape
-  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (evil-define-key '(normal motion insert) 'global (kbd "M-.") nil)
+  (evil-define-key '(normal motion insert) 'global (kbd "C-o") nil)
+  (evil-define-key '(normal insert) 'global (kbd "C-p") nil)
   (evil-set-undo-system 'undo-fu)
   (add-hook 'evil-insert-state-entry-hook #'jpdl/line-number-absolute)
   (add-hook 'evil-insert-state-exit-hook #'jpdl/line-number-relative)
   (setq evil-want-abbrev-expand-on-insert-exit nil)
-  :bind
-  (("C-s" . evil-write)
-   ("C-p" . evil-jump-forward)))
+  (define-key evil-normal-state-map (kbd "<left>") 'jpdl/bad-archer)
+  (define-key evil-normal-state-map (kbd "<right>") 'jpdl/bad-archer)
+  (define-key evil-normal-state-map (kbd "<down>") 'jpdl/bad-archer)
+  (define-key evil-normal-state-map (kbd "<up>") 'jpdl/bad-archer)
+  (evil-global-set-key 'motion (kbd "<left>") 'jpdl/bad-archer)
+  (evil-global-set-key 'motion (kbd "<right>") 'jpdl/bad-archer)
+  (evil-global-set-key 'motion (kbd "<down>") 'jpdl/bad-archer)
+  (evil-global-set-key 'motion (kbd "<up>") 'jpdl/bad-archer))
+
+(defun jpdl/bad-archer ()
+  (interactive)
+  (message "You have ran out of arrows!"))
 
 (defun jpdl/line-number-relative ()
   (setq display-line-numbers 'relative))
