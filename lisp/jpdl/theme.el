84101520c3dde0618b97014e730e260b53799e6b
João Lacerda <jpedrodelacerda@gmail.com>
2026-08-20 16:43:59 -0300

    fix(theme): broken `doom-theme` after updating to emacs-31.
    See https://github.com/doomemacs/themes/issues/875#issuecomment-3557150380.

--- lisp/jpdl/theme.el
+++ lisp/jpdl/theme.el
@@ -8,6 +8,8 @@
   :custom
   (custom-safe-themes t)
   :config
+  (setcdr (assoc 'gnus-group-news-low-empty doom-themes-base-faces)
+          '(:inherit 'gnus-group-mail-1-empty :weight 'normal))
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
