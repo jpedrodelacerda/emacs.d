9fee577c75fada3df78023fbdd0a40dbf2d59e3f
João Lacerda <jpedrodelacerda@gmail.com>
2026-08-20 23:20:16 -0300

    chore: remove `dap-mode`

--- lisp/jpdl/debug.el
+++ lisp/jpdl/debug.el
@@ -4,34 +4,6 @@
 ;; dap and dap-ui config
 
 ;;; Code:
-(use-package dap-mode
-  :straight t
-  :config
-  (setq dap-auto-configure-features '(sessions locals breakpoints expressions repl controls tooltip))
-  (dap-ui-mode 1)
-  (dap-tooltip-mode)
-  (require 'dap-gdb-lldb)
-  (setq dap-gdb-lldb-debug-program '("/usr/bin/lldb-vscode"))
-  (dap-register-debug-template
-   "Rust::LLDB Run Configuration"
-   (list :type "lldb"
-         :request "launch"
-         :name "LLDB::Run"
-	     :gdbpath "rust-lldb"
-         :target nil
-         :cwd nil))
-  :hook (prog-mode)
-  :init
-  (unbind-key "M-d")
-  :general
-  (:keymaps 'dap-mode-map
-            "<f5>" 'dap-debug
-            "<f6>" 'dap-hydra
-            "<f7>" 'dap-debug-restart
-            "M-d d" 'dap-hydra
-            "M-d b" 'dap-breakpoint-toggle
-            "M-d n" 'dap-next
-            "M-d h" 'dap-hydra))
 
 (provide 'jpdl/debug)
 ;;; debug.el ends here
